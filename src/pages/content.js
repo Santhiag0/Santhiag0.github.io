bc2e3b5c6baa18838271ed67587a9f1e5e854364
Thiago Lopez <jlopez2748@uta.edu.ec>
2023-11-29 16:50:56 -0500

    cambios

---
+++ src/pages/content.js
@@ -0,0 +1,102 @@
+// Personal Information --> #hero section
+export const name = "JOSUE LOPEZ"
+export const designation = "Estudiante Universitario"
+export const location = "Ambato, Ecuador"
+export const pronouns = "Thiago"
+export const website = "https://www.linkedin.com/in/thiag0/"
+
+// About
+export const about = "Soy un joven trabajador, un gran compañero de equipo y líder, me gusta aprender cosas nuevas, además de poder compartir mi experiencia y a su vez sumar nuevos conocimientos, destaco en el ámbito de ayudar y servir a los demás con una gran actitud, en especial me gusta mucho la tecnología, la administración y soy excelente hablando en público, estoy siempre dispuesto a aprender y colaborar."
+
+// Work Experience --> #work section
+export const work = [
+	{
+		from: "2001",
+		to: "Present",
+		role: "Safety Inspector",
+		org: "Quahog Safety Department",
+		org_url: "https://quahog.gov/safety",
+		location: "Quahog, Rhode Island",
+		skills: "Safety Inspection, Risk Assessment, Emergency Response"
+	}
+]
+
+// Projects --> #project section
+export const projects = [
+	{
+		title: "Gestor de Laboratorios UTA",
+		description: "Sistema para la gestión de Laboratorios de la UNIVERSIDAD TÉCNICA DE AMBATO",
+		url: "https://github.com/Santhiag0/Formularios",
+		tech: "Astro",
+	},
+	{
+		title: "My CV",
+		description: "Esta es una autoreferencia a esta página web",
+		url: "https://github.com/Santhiag0/Formularios",
+		tech: "Astro",
+	}
+]
+
+// Education --> #education section
+export const education = [
+	{
+		from: "2005",
+		to: "2007",
+		institute: "Unidad Educativa Nicolás Martínez",
+		course: "PRIMARIA",
+		location: "AMBATO"
+	},
+	{
+		from: "2007",
+		to: "2020",
+		institute: "Unidad Educativa Ambato",
+		course: "IB DIPLOMA PROGRAM",
+		location: "AMBATO"
+	},
+	{
+		from: "2021",
+		to: "ACTUALIDAD",
+		institute: "UNIVERSIDAD TÉCNICA DE AMBATO",
+		course: "INGENIERO EN SOFTWARE",
+		location: "AMBATO"
+	}
+]
+
+// Contact --> #contact section
+export const contact = [
+	{
+		source_name: "Email",
+		source: "jlopez2748@uta.edu.ec",
+	},
+	{
+		source_name: "Twitter",
+		source: "https://twitter.com/Josue",
+	},
+	{
+		source_name: "LinkedIn",
+		source: "https://www.linkedin.com/in/Santhiag0",
+	},
+	{
+		source_name: "WhatsApp",
+		source: "https://wa.link/bvnugy",
+	}
+]
+
+// Certifications --> #certificate section
+
+export const certificates = [
+	{
+		issued: "2022",
+		title: "Cisco Packet Tracer Introduction",
+		org: "CISCO",
+		url: "https://certi.com/url",
+		description: "Certificación por seguir el curso de CISCO PACKET TRACER",
+	},
+	{
+		issued: "2023",
+		title: "CSEI IV",
+		org: "UTA",
+		url: "https://certi.com/url",
+		description: "Aprobación por congreso CSEI IV",
+	},
+]
